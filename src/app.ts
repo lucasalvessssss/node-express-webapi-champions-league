bcb2b5f21308390b5826b78ed9cfb3a665ec3fa0
Lucas Alves <luc.antonio.lj@gmail.com>
2026-06-18 20:57:45 -0300

    update data and create some folders of project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+  import express, { json, Request, Response } from 'express';
+  
+function createApp() {
+    const app = express();
+
+  app.use(json());
+
+  app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ player: 'messi' });
+  });
+  return app;
+}
+
+export default createApp;
+
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-  console.log(`Server is running on port `);
+  import express, {json, Request, Response} from 'express';
+import createApp from './app';
+
+
+  const port = process.env.PORT;
+  const app = createApp();
+  
+  app.listen(port, () => {
+    console.log(`Server is running at port http://localhost:${port}`);
+  });
+
