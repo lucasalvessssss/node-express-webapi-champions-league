f42aa9226fec10b191ce0ceda1dce74e58604cb6
Lucas Alves <luc.antonio.lj@gmail.com>
2026-06-19 11:12:32 -0300

    updated files

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-  import express, { json, Request, Response } from 'express';
+  import express, { json, Request, Response, Router } from 'express';
+  import { getPlayers } from './controllers/player-controller';
+  import router from './routes';
   
 function createApp() {
     const app = express();
 
   app.use(json());
-
-  app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ player: 'messi' });
-  });
+  app.use("/api", router);
   return app;
 }
 
---
+++ src/services/players-service.ts
@@ -0,0 +1,16 @@
+import { noContent, ok } from "../utils/http-helper";
+
+
+export const getPlayerService = async () => {
+  const data = { player: 'messi' };
+  let response = null;
+
+  if(data){
+    response = await ok(data);
+  }
+  else{
+    response =await noContent();
+  }
+
+    return response ;
+};
